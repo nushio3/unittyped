cecafbc0532f17583a7d4e8de1dd703ba885973a
Takayuki Muranushi <muranushi@gmail.com>
2013-01-06 10:59:29 +0900

    make this test pass.

--- tests/TestUnbox.hs
+++ tests/TestUnbox.hs
@@ -16,12 +16,24 @@ import UnitTyped.SI.Show
 
 import qualified Prelude
 import Control.Monad (foldM, unless)
+import Data.Ratio
+import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
-import Prelude (zip, show, (++), IO, Bool(..), Integer, return, error, putStrLn)
+import Prelude (zip, show, (++), IO, Bool(..), Integer, Double, return, error, putStrLn, fromIntegral)
 import System.Exit (exitFailure)
 
+x1 :: Value LengthDimension (U Meter) Rational
+x1 = 1 *| meter
 
-t1 = hertz == (1 /| second)
+t1 = x1 == 72 *| centi meter + 280 *| mili meter
+
+
+vx0 :: V.Vector (Value LengthDimension (U Meter) Rational)
+vx0 = V.generate 120 (\i -> fromIntegral i *| meter)
+
+vux0d :: VU.Vector (Value LengthDimension (U Meter) Double)
+vux0d = VU.generate 120 (\i -> fromIntegral i *| meter)
+
 
 runTest :: Bool -> (Bool, Integer) -> IO Bool
 runTest b (True, _) = return b
