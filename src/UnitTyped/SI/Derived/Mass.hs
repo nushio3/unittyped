70f2ced30ed6eba717f1aaa88c41542c317ce0ff
Takayuki Muranushi <muranushi@gmail.com>
2013-02-07 17:11:24 +0900

    astronomical mass units added.

--- src/UnitTyped/SI/Derived/Mass.hs
+++ src/UnitTyped/SI/Derived/Mass.hs
@@ -34,6 +34,37 @@ instance Convertible Pound where
         type DimensionOf Pound = MassDimension 
 --
 
+-- | Earth mass, a unit for mass of the Sun.
+data EarthMass
+	deriving Typeable
+
+instance Convertible EarthMass where
+	factor _ = 5.97219e24
+	showunit _ = "M⊕"
+        type DimensionOf EarthMass = MassDimension 
+--
+
+-- | Solar mass, a unit for mass of the Sun.
+data SolarMass
+	deriving Typeable
+
+instance Convertible SolarMass where
+	factor _ = 1.98855e30
+	showunit _ = "M☉"
+        type DimensionOf SolarMass = MassDimension 
+--
+
+
+
 -- |One pound (lb).
 pound :: (Fractional f) => Value MassDimension (U Pound) f
 pound = one
+
+
+-- |One Earth mass.
+earthMass :: (Fractional f) => Value MassDimension (U EarthMass) f
+earthMass = one
+
+-- |One Solar mass.
+solarMass :: (Fractional f) => Value MassDimension (U SolarMass) f
+solarMass = one
